ab842bedeaa8f4b6f23bcc3e2c403507dd971eab
simGudim <simongudim@yahoo.ca>
2021-11-02 17:40:36 -0400

    added new fieldS

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .utils.database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ARRAY
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -7,7 +7,10 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key = True, index = True)
     username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    date_of_birth = Column(String)
     password = Column(String)
     email = Column(String)
     description = Column(String)
-    followers_id = Column([Integer], [ForeignKey("users.id")])
+    followers_id = Column(ARRAY(Integer))
